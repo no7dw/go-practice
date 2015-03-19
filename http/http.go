15e37bc4bfb4c0d2680c66fb3a4f94179f231642
no7dw <no7david@gmail.com>
2015-03-19 12:32:26 +0800

    still has bug when write string not long enough ,will keep some data left

--- http/http.go
+++ http/http.go
@@ -1,67 +1,76 @@
 package main
 
 import (
-	// "io"
-	"net/http"
-	"os"
-	"fmt"
-	"log"
-	"encoding/json"
-	
+    // "io"
+    "net/http"
+    "os"
+    "fmt"
+    "log"
+    "encoding/json"
+    // "strings"
     "io/ioutil"  
 )
 type Server struct {
-	ServersIP string
+    ServersIP string
 }
 func read()(string) {
-	file, err := os.Open("ip.log") // For read access.
-	if err != nil {
-		log.Fatal(err)
-	}	
-	data := make([] byte , 20)	
-	count, err := file.Read(data)
-	if err != nil {
-		log.Fatal(err)
-	}
-	ip := string(data[:count-1])
-	fmt.Printf("read : %s %d\n", ip, count)
-	defer file.Close()
-	return "{ \"ip\":\"" + ip + "\"}" 
+    file, err := os.Open("ip.log") // For read access.
+    if err != nil {
+        log.Fatal(err)
+    }   
+    data := make([] byte , 16)  
+    count, err := file.Read(data)
+    if err != nil {
+        log.Fatal(err)
+    }
+    ip := string(data[:count])
+    fmt.Printf("read : %s %d\n", ip, count)
+    defer file.Close()
+    return "{ \"ip\":\"" + ip + "\"}" 
 
 }
-func getip(w http.ResponseWriter, r *http.Request) {
-	ip := read()
-	w.Header().Set("Content-Type", "application/json")	
-	fmt.Fprint(w, ip)
+func getip(w http.ResponseWriter, r *http.Request) {        
+    ip := read()
+    w.Header().Set("Content-Type", "application/json")  
+    fmt.Fprint(w, ip)
 }
 func iphandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET"{
-		getip(w,r)
-	}else if r.Method == "POST"{
-		result, _:= ioutil.ReadAll(r.Body)  
-        r.Body.Close()  
-        fmt.Println(result)        
+    if r.Method == "GET"{
+        getip(w,r)
+    }else if r.Method == "POST"{
+        result, _:= ioutil.ReadAll(r.Body)  
+        r.Body.Close() 
+        // log.Println(string(result))         
+        // fmt.Println(result)        
         var s Server;  
-        json.Unmarshal([]byte(result), &s)  
-        file, err := os.Open("ip.log")
+        json.Unmarshal([]byte(result), &s) 
+
+        fmt.Println("save ip:",s.ServersIP)
+
+        f, err := os.OpenFile("ip.log",  os.O_WRONLY|os.O_CREATE, 0666)
         if err != nil {
-        	fmt.Println("open")
-			log.Fatal(err)
-		}
-		count, errw := file.WriteString(s.ServersIP)
-		if errw != nil {
-			fmt.Println("WriteString")
-			log.Fatal(errw)
-		}
-		fmt.Println("write: %d",count);
-		defer file.Close()        
-        fmt.Println(s.ServersIP);  
+            fmt.Printf("Error: Can't create config file, %s\n", err)
+            return
+        }
+        defer f.Close()
+         
+        str := s.ServersIP        
+        
+        n, err := f.WriteString(str)
+        if n != len(str) {
+            if err == nil {
+                panic("err ought to be something")
+            }
+            fmt.Printf("Error: Write has failed: %s\n", err) //this is the output, not the panic
+            return
+        }
+       
      }
 }
 
 func main() {
-	
-	http.HandleFunc("/", getip)
-	http.HandleFunc("/ip", iphandler)
-	http.ListenAndServe(":8000", nil)
+    
+    http.HandleFunc("/", getip)
+    http.HandleFunc("/ip", iphandler)
+    http.ListenAndServe(":8000", nil)
 }
