567382eb84435dac20aae96b11d0fbb863045b36
no7dw <no7david@gmail.com>
2015-03-26 00:55:59 +0800

    less blank

--- channel.go
+++ channel.go
@@ -1,26 +1,19 @@
 package main
-
 import (
 	"fmt"
 	"time"
 )
-
-
-
 func main(){
 	// list :=[5]int {1,2,3,4,5}
 	c := make(chan int) //allocate a channel
-
 	go func() {
 		// list.Sort()
 		time.Sleep(time.Duration(2) * time.Second)	
 		fmt.Println("Sort complete, send signal")
 		c <- 1 //send a signal, whatever value
 	}()
-
 	//do somethins for a while
 	time.Sleep(time.Duration(1) * time.Second)
-	//try time.Sleep(time.Duration(3h) * time.Second)
 	fmt.Println("waiting...")
 	//will block
 	value := <-c //wait for sort to finish , discard send value	
