54b8cd0a431deed3a2f9f9fb55b5f6c78448c484
no7dw <no7david@gmail.com>
2016-09-12 17:36:40 +0800

    try ignore result

--- withoutHandleError.go
+++ withoutHandleError.go
@@ -4,6 +4,7 @@ import "os"
 
 func main() {
   // ' !=  ""
+  // f := os.Open("README.md") //build error: return two parameter
   f, err := os.Open("README.md") //build error: err not used
   
   defer f.Close() //will however run this line of code
