8075e9a3d3bd66a4f9a4ed21beeb50fe28fa8d45
no7dw <no7david@gmail.com>
2018-08-28 10:05:44 +0800

    struct and oo

--- struct.go
+++ struct.go
@@ -1,13 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+ 	"strconv"
+)
+ 
+type People struct {
+	idcard string
+}
 
 type Employee struct {
+	people People
 	name string
 	age  int
 }
 
+func (p People) sayHi() string{
+	return "hello"
+}
+
+func (e Employee) sayHi() string {
+	return "hi"
+}
+
+func (e Employee) selfIntro() string {
+	return strconv.Itoa(e.age)
+}
+
 func main() {
-	e := Employee{"dengwei", 32}
-	fmt.Println(e)
+	p := People{"440184"}
+	e := Employee{People{"440183"},"dengwei", 32}
+	e1 := Employee{p,"dengwei2", 32}
+	fmt.Println(p,e,e1)
+	fmt.Println(p.sayHi())
+	fmt.Println(e.sayHi())
+	fmt.Println(e.selfIntro())
 }
