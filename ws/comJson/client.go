8a76461b9a7449c1a61de17b4a4e961f8718df2a
no7dw <no7david@gmail.com>
2018-08-30 12:02:36 +0800

    communicate json

---
+++ ws/comJson/client.go
@@ -0,0 +1,104 @@
+// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+package main
+
+import (
+	"flag"
+	"log"
+	"encoding/json"
+	// "math/rand"
+	"net/url"
+	"os"
+	"os/signal"
+	// "time"
+	// "fmt"
+	"github.com/gorilla/websocket"
+)
+
+type Employee struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+var addr = flag.String("addr", "localhost:8080", "http service address")
+
+func main() {
+	flag.Parse()
+	log.SetFlags(0)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			mt, message, err := c.ReadMessage()
+			log.Println("read:", err, message, mt)
+			var emp Employee
+			err = json.Unmarshal(message, &emp)
+			if err != nil {
+				log.Println("read:", err, mt)
+				return
+			}
+			// log.Printf("recv: %s", string(message[:]))
+			log.Printf("recv", emp)
+		}
+	}()
+
+	// ticker := time.NewTicker(time.Second)
+	
+	// defer ticker.Stop()
+	for {
+		// mt, message, err := c.ReadMessage()
+		// if err != nil {
+		// 	log.Println("read2", err)
+		// 	break
+		// }
+		// log.Println("message",mt, string(message[:]))
+		select {
+		// case <-done:
+		// 	return
+		// case t := <-ticker.C:
+		// // case	t := rand.ExpFloat64():
+		// 	// s := fmt.Sprintf("%f", t)
+		// 	// err := c.WriteMessage(websocket.TextMessage, []byte(s))
+		// 	err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
+
+			
+		// 	if err != nil {
+		// 		log.Println("write:", err)
+		// 		return
+		// 	}
+		case <-interrupt:
+			log.Println("interrupt")
+
+			// Cleanly close the connection by sending a close message and then
+			// waiting (with timeout) for the server to close the connection.
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close:", err)
+				return
+			}
+		// 	select {
+		// 	case <-done:
+		// 	case <-time.After(time.Second):
+		// 	}
+		// 	return
+		}
+	}
+}
---
+++ ws/comJson/server.go
@@ -0,0 +1,116 @@
+// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"github.com/gorilla/websocket"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"time"
+	"encoding/binary"
+	"bytes"
+	// "strconv"
+)
+type Employee struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func getDB(uri string) *mgo.Database {
+	session, err := mgo.Dial(uri)
+	if err != nil {
+		panic(err)
+	}
+	//defer session.Close()
+	c := session.DB("test")
+	return c
+}
+
+
+
+
+var addr = flag.String("addr", "localhost:8080", "http service address")
+
+var upgrader = websocket.Upgrader{} // use default options
+
+
+func find(dbc mgo.Database) (Employee , error){
+	c := dbc.C("employee")
+	result := Employee{}
+	err := c.Find(bson.M{"name": "wade"}).One(&result)
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+	return result, err
+}
+
+func getEmpFromDB() Employee{
+	
+	emp, err := find(*db)
+	if err != nil {
+		log.Println(emp)
+	}
+	// log.Println(emp,err)
+	return emp
+}
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	defer c.Close()
+	// counter := 1
+	for {
+		// mt, message, err := c.ReadMessage()
+		// if err != nil {
+		// 	log.Println("read:", err)
+		// 	break
+		// }
+		// log.Printf("recv: %s", message)
+		
+		// message := "abc"
+		
+
+		mt := 1 //TextMessage = 1 BinaryMessage = 2
+		select {
+			case <-ticker.C:
+				// message := Employee{"dengwei", 32}
+				// message += strconv.Itoa(counter)
+				// counter++
+				message := getEmpFromDB()
+				var bin_buf bytes.Buffer
+				binary.Write(&bin_buf, binary.LittleEndian, message)
+
+				err = c.WriteMessage(mt, bin_buf.Bytes())
+				if err != nil {
+					log.Println("write:", err)
+					break
+				}
+				log.Println("write:" , message)
+		}
+	}
+}
+
+var db *mgo.Database
+
+func main() {
+	db = getDB("localhost:27017")
+	flag.Parse()
+	log.SetFlags(0)
+	http.HandleFunc("/echo", echo)
+	// http.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
+
